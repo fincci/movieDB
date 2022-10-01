eecff55d7169c8ca629206e32acc3519b6595d49
filipe carvalho <filiperoc@hotmail.com>
2022-09-30 21:25:31 -0300

    fix null post / add favicon

--- src/components/movie-card/movie-card.js
+++ src/components/movie-card/movie-card.js
@@ -1,13 +1,28 @@
 import './movie-card.css'
+import poster_dafault from '../../assets/images/MTS.png'
 
 const MovieCard = ({ movieInfo, index }) => {
 
     const { poster_path, title } = movieInfo
-    return (
-        <li className="movie-card" key={index}>
-            <img className="poster" src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={`Poster de ${title}`} />
-        </li>
-    )
+
+    if (poster_path === null) {
+        return (
+            <li className="movie-card" key={index}>
+                <img className='poster' src={poster_dafault} alt={`(${title})`} />
+                <h3 className='movie-title'>{title}</h3>
+            </li>
+        )
+    } else {
+        return (
+            <li className="movie-card" key={index}>
+                <img className="poster" src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={`Poster de ${title}`} />
+            </li>
+        )
+    }
+
+
+
+
 }
 
 export { MovieCard }
